d6728d43ab3d9de782ac6dcc725662588885282d
vivekubhadiya <143770316+vivekubhadiya@users.noreply.github.com>
2024-02-18 17:33:44 -1200

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo {
+    // This \f escape sequence is a form feed character
+    // It is an old technique and used to indicate a page break.
+    static {
+        System.out.println("Meet");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Good Morning Geeks! \r How are you all? ");
+
+    }
+
+}
